055a5bdf60b8074d7c0d1bbbd091ea88806b4297
ashxraza <adarsh.radhesh@ada.ac.uk>
2026-04-21 10:49:57 +0000

    tasks complete

--- 1_temperature_converter.py
+++ 1_temperature_converter.py
@@ -1,8 +1,24 @@
-def celsius_to_fahrenheit(celsius):
-    fahrenheit = (celsius * 9/5) + 32
-    return fahrenheit
+# def celsius_to_fahrenheit(celsius):
+#      fahrenheit = (celsius * 9/5) + 32
+#      return fahrenheit
 
-# Test the function
-print(celsius_to_fahrenheit(0))
-print(celsius_to_fahrenheit(20))
-print(celsius_to_fahrenheit(100))
+# def fahrenheit_to_celsius(fahrenheit):
+#     celsisus = (fahrenheit - 32) * 1.8
+#     return celsisus
+
+# # Test the function
+# print(celsius_to_fahrenheit(-0))
+# print(celsius_to_fahrenheit(20))
+# print(celsius_to_fahrenheit(100))
+
+# print(fahrenheit_to_celsius(30))
+# print(fahrenheit_to_celsius(80))
+# print(fahrenheit_to_celsius(160))
+
+def kilometres_to_miles(kilometres):
+     return kilometres * 0.621371
+     
+
+# print(kilometres_to_miles(160))
+result = kilometres_to_miles(200)
+print(result)
--- 2_shopping_calculator.py
+++ 2_shopping_calculator.py
@@ -1,10 +1,17 @@
-def calculate_total(price, tax_rate=0.20, discount=0):
-    subtotal = price - discount
-    tax = subtotal * tax_rate
-    total = subtotal + tax
-    return total
+# def calculate_total(price, tax_rate=0.20, discount=0, tip=0.12):
+#     subtotalb4tip = price - discount
+#     subtotal = (subtotalb4tip * tip) + subtotalb4tip
+#     tax = subtotal * tax_rate
+#     total = subtotal + tax 
+#     return total
 
-# Test cases
-print(f"£{calculate_total(100):.2f}")
-print(f"£{calculate_total(100, 0.1):.2f}")
-print(f"£{calculate_total(100, 0.08, 10):.2f}")
+# # Test cases
+# print(f"£{calculate_total(100):.2f}")
+# print(f"£{calculate_total(100, 0.1):.2f}")
+# print(f"£{calculate_total(100, 0.08, 10):.2f}")
+
+
+def grade_calculator(homework_score, test_score):
+    return (homework_score * 0.30) + (test_score * 0.70)
+
+print(f"Grade is {grade_calculator(80,90):.2f}")
