a7161062ab300fcdc2e8741839e933184f2618ff
ashxraza <adarsh.radhesh@ada.ac.uk>
2026-04-21 14:11:44 +0000

    Password genarator acitivity done

--- 3_check_password_strength.py
+++ 3_check_password_strength.py
@@ -1,3 +1,5 @@
+import random
+
 def check_password_strength(password):
     score = 0
     feedback = []
@@ -30,11 +32,31 @@ def check_password_strength(password):
     
     return score, feedback
 
+
+def strength_rating(score):
+    if score >= 4:
+        return "Strong password"
+    elif score >= 2:
+        return "Sub optimal password"
+    return "Weak password"
+
+
+def generate_password():
+    password_length = random.randint(8,12)
+    uppercase_characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase_chacters = "abcdefghijklmnopqrstuvwxyz"
+    numbers = "1234567890"
+    special_characters = "!@#$%^&*"
+    passwords_pick = 
+
+
+
 # Test passwords
 passwords = ["hello", "Hello123", "PASSWORD", "MyPass123!"]
 for pwd in passwords:
     score, issues = check_password_strength(pwd)
     print(f"'{pwd}': Score {score}/5")
+    print(strength_rating(score))
     for issue in issues:
         print(f"  - {issue}")
     print()
